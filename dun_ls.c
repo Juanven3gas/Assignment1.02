45a74471551df0871f3f69a4b28f12be63108f31
Juan Venegas <juanven3gas@gmail.com>
2017-09-12 21:28:02 -0500

    adding notes & trying to read binary file

--- dun_ls.c
+++ dun_ls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <endian.h>
 #include "dun_utils.h"
 
 int main (int argc, char * argv[])
@@ -8,7 +9,8 @@ int main (int argc, char * argv[])
     int flag_save = 0;
     int flag_load = 0;
     int arguement_index = 1;
-    char *filePath;
+    char *home  = getenv("HOME");
+    FILE *f;
 
     if(argc < 1)
     {
@@ -45,19 +47,64 @@ int main (int argc, char * argv[])
     else if(flag_load)
     {
         //TODO
-        filePath = getenv("HOME");
-        strcat(filePath, ".rlg327/dungeon");
-        printf("filepath: %s\n", filePath);
         printf("Program will only load\n");
+        int length = strlen(home) + strlen("/.rlg327/dungeon") + 1;
+        char * path;
+        char * buffer;
+        int f_size;
+        size_t result;
+
+        path = malloc(length);
+        strcpy (path, home);
+        strcat(path, "/.rlg327/dungeon");
+        f = fopen(path, "rb");
+
+        if (f == NULL)
+        {
+            fputs("File Error", stderr);
+            exit(1) ;
+        }
+
+        //Get file size
+        fseek(f, 0, SEEK_END);
+        f_size = ftell(f);
+        rewind(f);
+
+        //allocate memory to contain whole file
+        buffer = (char*) malloc (sizeof(char)*f_size);
+        if(buffer == NULL)
+        {
+            fputs("Memory error", stderr);
+            exit (2);
+        }
+
+        //copy file to buffer
+        result = fread(buffer, 1, f_size, f);
+        if(result != f_size)
+        {
+            fputs ("Reading error", stderr);
+            exit (3);
+        }
+        
+        
+        printf("size of file: %d & calc size: %d\n", f_size, num);
+        free(path);
+        free(buffer);
+        fclose(f);
     }
     else if(flag_save)
     {
         //TODO
-
-        filePath = getenv("HOME");
-        strcat(filePath, ".rlg327/dungeon");
-        printf("filepath: %s\n", filePath);
         printf("Program will only save\n");
+        int length = strlen(home) + strlen("/.rlg327/dungeon") + 1;
+        char * path;
+        path = malloc(length);
+        strcpy (path, home);
+        strcpy(path, "/.rlg327/dungeon");
+        f = fopen(path, "rw");
+        free(path);
+        fclose(f);
+        
     }
     else 
     {
